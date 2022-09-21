d494ec3097dd50a7b7aeca875bf25f5e2718f832
Thiago Falasca <thiagofduarte12@gmail.com>
2022-09-21 00:30:58 -0300

    Atualizando Ações.py

--- acoes.py
+++ acoes.py
@@ -11,128 +11,131 @@ with open("acoes.txt") as f:
 acoes = [x.strip("\n") for x in acoes]
 
 file = open("indicadoresAcoes.csv", "w", encoding="UTF-8")
-file.write('"Empresa","Código","SubSetor","Segmento","Valor Atual","D.Y","P/L","PEG RATIO","VPA","P/VP","Liq Corrente","Dív.Liquida/EBIT","Margem Líquida","ROE","ROIC","CAGR Receitas","CAGR Lucros"\n')
+file.write('"Empresa","Código","Segmento","Valor Atual","D.Y","P/L","P/VP","Peg Ratio","LPA","VPA","DivLiq/PL","DivLiq/Ebitda","Liq Corrente","ROE","ROIC","CAGR R","CAGR L","Mrg. Bruta","Mrg. Líquida","Mrg. Ebitda"\n')
 file.close()
 
 for line in acoes:
-
-    for i in range(3):
-        url = "https://www.google.com/search?q=ações+" + line.strip().lower()
+    codeAcao = line.strip().upper()
+    try:
+        print(codeAcao)
+        if codeAcao.find("34") > 0:
+            url = "https://statusinvest.com.br/bdrs/" + codeAcao
+        else:
+            url = "https://statusinvest.com.br/acoes/" + codeAcao
         site = requests.get(url, headers=headers)
         soup = BeautifulSoup(site.content, "html.parser")
+        dom = etree.HTML(str(soup))
+        nomeAcao = dom.xpath('//*[@id="main-header"]/div[2]/div/div[1]/h1/small')[
+            0
+        ].text
+        valorAtual = dom.xpath(
+            '//*[@id="main-2"]/div[2]/div/div[1]/div/div[1]/div/div[1]/strong'
+        )[0].text
+        dy = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[1]/div/div/strong'
+        )[0].text
+        pl = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[2]/div/div/strong'
+        )[0].text
+        pvp = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[4]/div/div/strong'
+        )[0].text
+        pegRatio = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[3]/div/div/strong'
+        )[0].text
+        lpa = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[11]/div/div/strong'
+        )[0].text
+        vpa = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[9]/div/div/strong'
+        )[0].text
+        divPL = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[2]/div/div[1]/div/div/strong'
+        )[0].text
+        divEbitda = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[2]/div/div[2]/div/div/strong'
+        )[0].text
+        liqCorrente = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[2]/div/div[6]/div/div/strong'
+        )[0].text
+        roe = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[4]/div/div[1]/div/div/strong'
+        )[0].text
+        roic = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[4]/div/div[3]/div/div/strong'
+        )[0].text
+        cagrR = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[5]/div/div[1]/div/div/strong'
+        )[0].text
+        cagrL = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[5]/div/div[2]/div/div/strong'
+        )[0].text
+        mBruta = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[3]/div/div[4]/div/div/strong'
+        )[0].text
+        mLiq = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[3]/div/div[4]/div/div/strong'
+        )[0].text
+        mEbitda = dom.xpath(
+            '//*[@id="indicators-section"]/div[2]/div/div[3]/div/div[4]/div/div/strong'
+        )[0].text
         try:
-            codeAcao = (
-                soup.find("div", class_="wx62f PZPZlf x7XAkb").get_text().split(
-                    ": ")[1]
-            )
-            print(codeAcao)
-            if codeAcao.find("34") > 0:
-                url = "https://statusinvest.com.br/bdrs/" + codeAcao
-            else:
-                url = "https://statusinvest.com.br/acoes/" + codeAcao
-            site = requests.get(url, headers=headers)
-            soup = BeautifulSoup(site.content, "html.parser")
-            dom = etree.HTML(str(soup))
-            nomeAcao = dom.xpath('//*[@id="main-header"]/div[2]/div/div[1]/h1/small')[
-                0
-            ].text
-            valorAtual = dom.xpath(
-                '//*[@id="main-2"]/div[2]/div/div[1]/div/div[1]/div/div[1]/strong'
+            segmento = dom.xpath(
+                '//*[@id="company-section"]/div[1]/div/div[3]/div/div[3]/div/div/div/a/strong'
             )[0].text
-            dy = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[1]/div/div/strong'
-            )[0].text
-            pl = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[2]/div/div/strong'
-            )[0].text
-            pegratio = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[3]/div/div/strong'
-            )[0].text
-            vpa = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[9]/div/div/strong'
-            )[0].text
-            pvp = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[1]/div/div[4]/div/div/strong'
-            )[0].text
-            liqCorrente = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[2]/div/div[6]/div/div/strong'
-            )[0].text
-            divEbit = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[2]/div/div[3]/div/div/strong'
-            )[0].text
-            mLiq = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[3]/div/div[4]/div/div/strong'
-            )[0].text
-            roe = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[4]/div/div[1]/div/div/strong'
-            )[0].text
-            roic = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[4]/div/div[3]/div/div/strong'
-            )[0].text
-            cagrR = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[5]/div/div[1]/div/div/strong'
-            )[0].text
-            cagrL = dom.xpath(
-                '//*[@id="indicators-section"]/div[2]/div/div[5]/div/div[2]/div/div/strong'
-            )[0].text
-            try:
-                subSetor = dom.xpath(
-                    '//*[@id="company-section"]/div[1]/div/div[3]/div/div[2]/div/div/div/a/strong'
-                )[0].text
-                segmento = dom.xpath(
-                    '//*[@id="company-section"]/div[1]/div/div[3]/div/div[3]/div/div/div/a/strong'
-                )[0].text
-            except:
-                subSetor = dom.xpath(
-                    '//*[@id="company-section"]/div[1]/div[3]/div/div[2]/div/div/div/a/strong'
-                )[0].text
-                segmento = dom.xpath(
-                    '//*[@id="company-section"]/div[1]/div[3]/div/div[3]/div/div/div/a/strong'
-                )[0].text
-
-            with open("indicadoresAcoes.csv", "a", newline="", encoding="UTF-8") as r:
-                linha = (
-                    '"'
-                    + nomeAcao
-                    + '","'
-                    + codeAcao
-                    + '","'
-                    + subSetor
-                    + '","'
-                    + segmento
-                    + '","'
-                    + valorAtual
-                    + '","'
-                    + dy
-                    + '","'
-                    + pl
-                    + '","'
-                    + pegratio
-                    + '","'
-                    + vpa
-                    + '","'
-                    + pvp
-                    + '","'
-                    + liqCorrente
-                    + '","'
-                    + divEbit
-                    + '","'
-                    + mLiq
-                    + '","'
-                    + roe
-                    + '","'
-                    + roic
-                    + '","'
-                    + cagrR
-                    + '","'
-                    + cagrL
-                    + '"'
-                    + "\n"
-                )
-                r.write(linha)
-                break
         except:
-            if i == 2:
-                print("Acao " + line + " nao encontrada!")
+            segmento = dom.xpath(
+                '//*[@id="company-section"]/div[1]/div[3]/div/div[3]/div/div/div/a/strong'
+            )[0].text
+
+        with open("indicadoresAcoes.csv", "a", newline="", encoding="UTF-8") as r:
+            linha = (
+                '"'
+                + nomeAcao
+                + '","'
+                + codeAcao
+                + '","'
+                + segmento
+                + '","'
+                + valorAtual
+                + '","'
+                + dy
+                + '","'
+                + pl
+                + '","'
+                + pvp
+                + '","'
+                + pegRatio
+                + '","'
+                + lpa
+                + '","'
+                + vpa
+                + '","'
+                + divPL
+                + '","'
+                + divEbitda
+                + '","'
+                + liqCorrente
+                + '","'
+                + roe
+                + '","'
+                + roic
+                + '","'
+                + cagrR
+                + '","'
+                + cagrL
+                + '","'
+                + mBruta
+                + '","'
+                + mLiq
+                + '","'
+                + mEbitda
+                + '"'
+                + "\n"
+            )
+            r.write(linha)
+    except:
+        print("Acao " + line + " nao encontrada!")
 
 r.close()
+
